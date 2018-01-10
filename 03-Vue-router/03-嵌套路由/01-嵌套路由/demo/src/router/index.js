d48561109b18a11f180f848a51456e61d83c8eb1
245967906 <245967906@qq.com>
2018-01-10 17:06:47 +0800

    03-Vue-router/03-嵌套路由/01-嵌套路由

---
+++ 03-Vue-router/03-嵌套路由/01-嵌套路由/demo/src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/views/Index'
+import Product from '@/views/Product'
+import Order from '@/views/Order'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index,
+      children: [
+        {
+          path: '',
+          component: Product
+        },
+        {
+          path: 'product',
+          component: Product
+        },
+        {
+          path: 'order',
+          component: Order
+        }
+      ]
+    }
+  ]
+})
---
+++ 03-Vue-router/03-嵌套路由/01-嵌套路由/demo/src/views/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>首页</p>
+    <router-link to="/product">显示产品页</router-link>
+    <router-link to="/order">显示订单页</router-link>
+    <router-view></router-view>
+  </div>
+</template>
