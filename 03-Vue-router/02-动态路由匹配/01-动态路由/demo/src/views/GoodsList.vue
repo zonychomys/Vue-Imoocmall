501a4d613b71c4f1d3c30eee2550c57d55fd3cfd
245967906 <245967906@qq.com>
2018-01-10 15:16:20 +0800

    03-Vue-router/02-动态路由匹配/01-动态路由

---
+++ 03-Vue-router/02-动态路由匹配/01-动态路由/demo/src/views/GoodsList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    这是商品列表页面:
+    <span>{{ $route.params.goodsId }}</span>
+    <span>{{ $route.params.name }}</span>
+  </div>
+</template>
+
+<style></style>
+
+<script></script>
